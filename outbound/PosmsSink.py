8ee2faa7b0fdb2502395bcb23d81bd51eb586143
Bill <shirunjie@126.com>
2020-04-05 12:46:14 -0400

    Clarify error message (when finger is out, no repeated message is sent)

--- outbound/PosmsSink.py
+++ outbound/PosmsSink.py
@@ -22,7 +22,7 @@ class PosmsSink(OutboundSink):
                     'API_KEY': self.config["settings"]["api_key"],
                     'DEV_ID': self.device_info.get("id", "unknown_id"), 
                     'DEV_TYPE': self.device_info.get("type", "unknown_type"),
-                    'DEV_MSG': 'Error/Status unavailable',
+                    'DEV_MSG': 'Error/Status unavailable or finger out',
                     'DEV_LOC': self.device_info.get("location", "unknown_location"),
                     'RECORD_TIME': datetime.datetime.now(),
                     'O2_VAL': ERROR_UNSET,
